f53628975be3f4b1f5d067f04b0697abc839bb23
Mihir Wadekar <mwadekar2000@gmail.com>
2023-03-06 22:59:23 -0800

    Added 3D array tests

--- handout/tests/test3.c
+++ handout/tests/test3.c
@@ -3,8 +3,8 @@
 int main() {
   int numbers[3][4][5];
   for(int i = 0; i < 3; i++) {
-    for (int j = 0; j < 4; j++) {
-      for (int k = 0; k < 6; k++) {
+    for (int j = 0; j < 5; j++) {
+      for (int k = 0; k < 5; k++) {
         numbers[i][j][k] = k;
       }
     }
