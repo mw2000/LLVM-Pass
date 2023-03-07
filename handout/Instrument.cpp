28e18e25c07556a571995b0fc0c20db2a63c7324
Chaitenya Gupta <48187468+Chaitenyag@users.noreply.github.com>
2023-03-07 11:51:14 -0800

    Update Instrument.cpp
    fixed typo

--- handout/Instrument.cpp
+++ handout/Instrument.cpp
@@ -16,52 +16,52 @@ bool Instrument::runOnModule(Module &M) {
 
 
 bool Instrument::runOnFunction(Module &M, Function &F) {
+	for (Function::iterator b = F.begin(), be = F.end(); b != be; b++) {
+		for (BasicBlock::iterator i = b->begin(), ie = b->end(); i != ie; i++) {      
+			Instruction *inst = (Instruction*)i;
+			/***
+			** STEP-1: 
+			** Reimplement the following if-condition statement to check
+			** for the instructions of interest (array accesses).
+			***/
+			if (GetElementPtrInst *getElement = dyn_cast<GetElementPtrInst>(inst)) {
+				Type *type = getElement->getSourceElementType();
+				//if we are looking at an array evaluate to true
+				if (ArrayType *arr = dyn_cast<ArrayType>(type)) {
 
-  for (Function::iterator b = F.begin(), be = F.end(); b != be; b++) {
-    for (BasicBlock::iterator i = b->begin(), ie = b->end(); i != ie; i++) {      
-      Instruction *inst = (Instruction*)i;
+					/***
+					** STEP-2: Retrieve the size of the array
+					**/
+					Value *ArraySize = llvm::ConstantInt::get(Type::getInt64Ty(M.getContext()), arr->getNumElements());
+					
+					/***
+					 ** STEP-3: Retrieve the index of the array element being accessed
+					**/
+					Value *Index = getElement->getOperand(2);
 
-      /***
-       ** STEP-1: 
-       ** Reimplement the following if-condition statement to check
-       ** for the instructions of interest (array accesses).
-       ***/
+					// 1: this is index ; 2: this is a register
+					errs() << "\narray_index: " << Index << "\n";
+					
+					/***
+				 	** STEP-4: Retrieve the source information of the instruction
+					 **/
 
-      if (true) {
-
-	/* Use the following to print the instruction to instrument for debugging purposes. */
-	errs() << "  Instruction: " << *inst << "\n";
-
-	/***
-	 ** STEP-2: Retrieve the size of the array
-	 **/
-  
-	/***
-	 ** STEP-3: Retrieve the index of the array element being accessed
-	 **/
-
-	/***
-	 ** STEP-4: Retrieve the source information of the instruction
-	 **/
-  
-	/***
-	 ** STEP-5: Create and store the arguments of function check_bounds
-	 ** in the vector args.
-	 **/
-	std::vector<Value*> args;
-
-	/***
-	 ** The code below creates and inserts the call before inst. Modify as needed.
-	 **/
-
-	Function *callee = M.getFunction("check_bounds");
-	if (callee) {
-	  CallInst::Create(callee, args, "", inst);
+					/***
+	 				** STEP-5: Create and store the arguments of function check_bounds
+					** in the vector args.
+					**/
+					std::vector<Value*> args;
+					args.push_back(ArraySize);
+					args.push_back(Index);
+					Function *callee = M.getFunction("check_bounds");
+					if (callee) {
+						CallInst::Create(callee, args, "", inst);
+					}
+				}
+			}
+		}
 	}
-      }
-    }
-  }
-  return false;
+	return false;
 }
 
 
