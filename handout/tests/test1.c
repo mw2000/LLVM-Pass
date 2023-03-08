67bea4ee34201003c66f0d5d40a2057e3919302c
Chaitenya Gupta <48187468+Chaitenyag@users.noreply.github.com>
2023-03-08 13:48:00 -0800

    failing test case

--- handout/tests/test1.c
+++ handout/tests/test1.c
@@ -1,8 +1,21 @@
 #include <stdio.h>
 
 int main() {
-  int x = 5;
-  int *arr[1];
-  *arr[1] = 30;
+  double arr[2][3] = {{2.2, 2.5, 1.4}, {5.5, 5.8, 9.7}};
+
+  // valid
+  for (int i = 0; i < 2; i++) {
+    for (int j = 0; j < 3; j++) {
+      double value = arr[i][j];
+    }
+  }
+
+  // invalid
+  for (int i = 0; i < 2; i++) {
+    for (int j = 0; j < 4; j++) {
+      double value = arr[i][j];
+    }
+  }
+
   return 0;
 }
