7567a0479efed1de62c9259471ab611732d18f50
Chaitenya Gupta <48187468+Chaitenyag@users.noreply.github.com>
2023-03-08 22:12:00 -0800

    changes tests comment

--- handout/tests/test18.c
+++ handout/tests/test18.c
@@ -1,10 +1,10 @@
-// 2D double array with out of bounds in 1st loop
+// 2D double array with out of bounds in 2nd loop
 #include <stdio.h>
 
 int main() {
   double arr[2][2] = {{8.2, 1.1}, {9, 14.7}};
-  for (int i = 0; i < 3; i++) {
-    for(int j = 0; j < 2; j++) {
+  for (int i = 0; i < 2; i++) {
+    for(int j = 0; j < 3; j++) {
       double value = arr[i][j];
     }
   }
--- handout/tests/test20.c
+++ handout/tests/test20.c
@@ -1,10 +1,10 @@
-// 2D double array with out of bounds in 1st loop
+// 2D double array with for loop out of bounds in both loops
 #include <stdio.h>
 
 int main() {
   double arr[2][2] = {{8.2, 1.1}, {9, 14.7}};
   for (int i = 0; i < 3; i++) {
-    for(int j = 0; j < 2; j++) {
+    for(int j = 0; j < 3; j++) {
       double value = arr[i][j];
     }
   }
